ddcc91fad06062b80769542b6aa117905e1b66ee
MetallicAlex <49795539+MetallicAlex@users.noreply.github.com>
2021-01-28 11:32:50 +0300

    thermalmap

--- statistic_visualization.py
+++ statistic_visualization.py
@@ -1,7 +1,8 @@
-from matplotlib import pyplot as plt, ticker
+from matplotlib import pyplot as plt, ticker, dates
 import pandas as pd
 import numpy as np
 import models
+import datetime as dt
 
 
 class Visualization:
@@ -9,7 +10,7 @@ class Visualization:
         pass
 
     @staticmethod
-    def show_plot_employee(employee_id, start_date=None, end_date=None):
+    def __statistics_from_database(employee_id, start_date=None, end_date=None):
         with models.get_session() as session:
             employee = session.query(models.Employee.name).filter(models.Employee.id == employee_id).scalar()
             if start_date and end_date is not None:
@@ -20,15 +21,32 @@ class Visualization:
             else:
                 statistics = session.query(models.Statistic.time, models.Statistic.temperature) \
                     .filter(models.Statistic.id_employee == employee_id).all()
-            times = np.array([time.strftime('%Y-%m-%d %H:%M:%S') for time, _ in statistics])
-            temperatures = np.array([float(temperature) for _, temperature in statistics])
-            df = pd.DataFrame(temperatures, index=times)
-            ax = plt.axes()
-            ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-            df.plot(kind='line', style='ko-', legend=False,
-                    xlabel='Datetime', ylabel='Temperature', title=employee, grid=True, ax=ax)
-            plt.xticks(rotation=45, horizontalalignment='right', fontweight='light', fontsize='x-small')
-            plt.show()
+        return statistics, employee
+
+    @staticmethod
+    def __statistic_to_dataframe(data):
+        statistics = {'x': [], 'y': [], 'date': [], 'time': [], 'temperature': []}
+        for datetime, temperature in data:
+            time = datetime.strptime(datetime.strftime('%H:%M'), '%H:%M')
+            statistics['date'].append(datetime.strftime('%Y-%m-%d'))
+            statistics['time'].append(datetime.strftime('%H:%M'))
+            statistics['temperature'].append(float(temperature))
+            statistics['y'].append(time.second + time.minute*60 + time.hour*3600)
+        statistics['x'] = dates.date2num(statistics['date'])
+        return pd.DataFrame(data=statistics).sort_index()
+
+    @staticmethod
+    def show_plot_employee(employee_id, start_date=None, end_date=None):
+        statistics, employee = Visualization.__statistics_from_database(employee_id, start_date, end_date)
+        times = np.array([time.strftime('%Y-%m-%d %H:%M:%S') for time, _ in statistics])
+        temperatures = np.array([float(temperature) for _, temperature in statistics])
+        df = pd.DataFrame(temperatures, index=times)
+        ax = plt.axes()
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        df.plot(kind='line', style='ko-', legend=False,
+                xlabel='Datetime', ylabel='Temperature', title=employee, grid=True, ax=ax)
+        plt.xticks(rotation=45, horizontalalignment='right', fontweight='light', fontsize='x-small')
+        plt.show()
 
     @staticmethod
     def show_plot_employees(employees_id, start_date=None, end_date=None):
@@ -40,15 +58,7 @@ class Visualization:
             fig, axes = plt.subplots(nrows=len(employees_id), ncols=1)
             fig.subplots_adjust(wspace=0.5, hspace=0.5)
             for index, employee_id in enumerate(employees_id):
-                employee = session.query(models.Employee.name).filter(models.Employee.id == employee_id).scalar()
-                if start_date and end_date is not None:
-                    statistics = session.query(models.Statistic.time, models.Statistic.temperature) \
-                        .filter(models.Statistic.id_employee == employee_id,
-                                models.Statistic.time >= start_date,
-                                models.Statistic.time <= end_date).all()
-                else:
-                    statistics = session.query(models.Statistic.time, models.Statistic.temperature) \
-                        .filter(models.Statistic.id_employee == employee_id).all()
+                statistics, employee = Visualization.__statistics_from_database(employee_id, start_date, end_date)
                 times = np.array([time.strftime('%Y-%m-%d') for time, _ in statistics])
                 temperatures = np.array([float(temperature) for _, temperature in statistics])
                 # Create plot
@@ -66,10 +76,21 @@ class Visualization:
 
     @staticmethod
     def show_temperature_map_employee(employee_id, start_date=None, end_date=None):
-        pass
+        statistics, employee = Visualization.__statistics_from_database(employee_id)
+        df = Visualization.__statistic_to_dataframe(statistics)
+        with plt.style.context('dark_background'):
+            ax = df.plot.hexbin(x='x', y='y', C='temperature', gridsize=10,
+                                cmap="coolwarm", title=employee, grid=False)
+            ax.set_xlabel("Date")
+            ax.set_ylabel("Time")
+            ax.xaxis.set_major_formatter(dates.DateFormatter('%Y-%m-%d'))
+            ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: str(dt.timedelta(seconds=x))))
+        plt.show()
 
 
 if __name__ == '__main__':
     Visualization.show_plot_employee(2)
     Visualization.show_plot_employees(employees_id=[1, 2, 3, 4], start_date='2021-01-17 00:00:00',
                                       end_date='2021-01-20 23:59:59')
+    Visualization.show_temperature_map_employee(1)
+    Visualization.show_temperature_map_employee(2)
