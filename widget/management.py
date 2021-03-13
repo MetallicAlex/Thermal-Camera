7dbbf2ccff078393ce11959f532b1f7f0703da95
MetallicAlex <49795539+MetallicAlex@users.noreply.github.com>
2021-03-14 00:07:42 +0300

    Add properties and device methods

--- widget/management.py
+++ widget/management.py
@@ -1,3 +1,7 @@
+import os
+import re
+import json
+import socket
 from typing import Union
 
 import widget.models as models
@@ -80,21 +84,21 @@ class DBManagement:
         with models.get_session() as session:
             if isinstance(low, str) and isinstance(high, str):
                 if not identifiers:
-                    self._statistics = session.query(models.Statistic)\
+                    self._statistics = session.query(models.Statistic) \
                         .filter(models.Statistic.time >= low,
-                                models.Statistic.time <= high)\
+                                models.Statistic.time <= high) \
                         .all()
                 else:
-                    self._statistics = session.query(models.Statistic)\
+                    self._statistics = session.query(models.Statistic) \
                         .filter(models.Statistic.id_profile.in_(identifiers) == models.Profile.id,
                                 models.Statistic.time >= low,
-                                models.Statistic.time <= high)\
+                                models.Statistic.time <= high) \
                         .all()
             elif isinstance(low, float) and isinstance(high, float):
                 if not identifiers:
-                    self._statistics = session.query(models.Statistic)\
+                    self._statistics = session.query(models.Statistic) \
                         .filter(models.Statistic.temperature >= low,
-                                models.Statistic.temperature <= high)\
+                                models.Statistic.temperature <= high) \
                         .all()
                 else:
                     self._statistics = session.query(models.Statistic) \
@@ -104,11 +108,11 @@ class DBManagement:
                         .all()
             else:
                 if not identifiers:
-                    self._statistics = session.query(models.Statistic)\
+                    self._statistics = session.query(models.Statistic) \
                         .all()
                 else:
-                    self._statistics = session.query(models.Statistic)\
-                        .filter(models.Statistic.id_profile.in_(identifiers))\
+                    self._statistics = session.query(models.Statistic) \
+                        .filter(models.Statistic.id_profile.in_(identifiers)) \
                         .all()
         return self._statistics
 
@@ -124,14 +128,67 @@ class DBManagement:
                 .delete(synchronize_session=False)
         self._statistics = None
 
+    def remove_statistics_duplicates(self):
+        pass
+
 
 class DeviceManagement:
     def __init__(self):
         self.DEVICE_TOKEN_DEBUG = '1057628122'
-        self.devices = None
+        self._path_file = os.path.dirname(os.path.abspath(__file__))
+        with open(f'{self._path_file}/data/devices.json') as file:
+            self._devices = json.load(file, strict=False)
+        self._host_name = None
+        self._host = None
+        self._port = None
+
+    @property
+    def host_name(self):
+        return self._host_name
+
+    @host_name.setter
+    def host_name(self, value):
+        self._host_name = value
+
+    @property
+    def host(self):
+        return self._host
+
+    @host.setter
+    def host(self, value):
+        self._host = value
+
+    @property
+    def port(self):
+        return self._port
+
+    @port.setter
+    def port(self, value):
+        self._port = value
+
+    @property
+    def devices(self):
+        return self._devices
 
     def add_device(self):
         pass
 
     def remove_device(self):
         pass
+
+    def save_devices(self):
+        with open(f'{self._path_file}/data/devices.json', 'w') as file:
+            json.dump(self.devices, file)
+
+    def find_devices(self):
+        os.system('chcp 437')
+        with os.popen(f'arp -a -N {self.host_ip}') as file:
+            data = file.read()
+        devices = []
+        for device in re.findall('([-.0-9]+)\s+([-0-9a-f]{17})\s+(\w+)', data):
+            if 'dynamic' in device:
+                devices.append(device[:2])
+
+    def find_host_info(self):
+        self.host_name = socket.gethostname()
+        self.host = socket.gethostbyname(self.host_name)
